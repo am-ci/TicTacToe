b5f809b8e0d4e166308da2861a893ea2426cc1a8
Amandine Cian <amandine.cian@gmail.com>
2022-12-15 11:50:06 +0100

    clic case

--- script-tictac.js
+++ script-tictac.js
@@ -2,9 +2,10 @@ let cells = document.querySelectorAll('.cell');
 
 for (let cell of cells) {
     cell.addEventListener('click', function(){
-        console.log('merdouille')
+        console.log(cell.dataset.game) //verif
 
     });
 
 }
 
+// cell.dataset.game // dataset.cell équivaut à data-game de mon HTML
