9b4d831838370d5366a1283af7073712bf86acaf
Amandine Cian <amandine.cian@gmail.com>
2022-12-15 16:43:58 +0100

    check partie

--- script-tictac.js
+++ script-tictac.js
@@ -1,12 +1,13 @@
 let cells = document.querySelectorAll('.cell');
 let state = ["","","","","","","","",""];
 let current = 'X';
+let win = false;
 
 
 for (let cell of cells) {
     cell.addEventListener('click', function(){
         console.log(cell.dataset.game) //verif
-        if (state[cell.dataset.game -1] == "") {
+        if (state[cell.dataset.game -1] == "" && win == false) {
             state[cell.dataset.game -1] = current
             cell.textContent = current
             console.log(state); //Verif
@@ -14,16 +15,81 @@ for (let cell of cells) {
             alert ("ERREUR")
         }
         
-        if (current == 'X') {
-            current = 'O';
-        } else {
-            current = 'X';
-        }
+        check();
+        // stop();
+
+        figure();
+        
+      
     });
+    
 
 }
 
+
 // cell.dataset.game // dataset.cell équivaut à data-game de mon HTML
 
+// On affichera donc un message avec le joueur qui a gagné ou si personne n'a gagné.
+
+// Passage X O
+function figure() {
+    if (current == 'X') {
+        current = 'O';
+    } else {
+        current = 'X';
+    }
+}
 
 
+//Vérification des conditions gagnantes
+function check() {
+    if (state[0] == current && state[1]== current && state[2] == current) {
+        win = true;
+        console.log(`joueur ${current} a gagné`);
+           
+    } else {
+        console.log('suivant'); //pour verif
+    }
+
+    if (state[3] == current && state[4]== current && state[5] == current) {
+        console.log(`joueur ${current} a gagné`);
+       
+    } 
+
+    if (state[6] == current && state[7]== current && state[8] == current) {
+        console.log(`joueur ${current} a gagné`);
+        
+    } 
+
+    if (state[0] == current && state[4]== current && state[6] == current) {
+        console.log(`joueur ${current} a gagné`);
+        
+    } 
+
+    if (state[1] == current && state[4]== current && state[7] == current) {
+        console.log(`joueur ${current} a gagné`);
+        
+    } 
+
+    if (state[2] == current && state[5]== current && state[8] == current) {
+        console.log(`joueur ${current} a gagné`);
+        
+    } 
+
+    if (state[0] == current && state[4]== current && state[8] == current) {
+        console.log(`joueur ${current} a gagné`);
+        
+    } 
+
+    if (state[2] == current && state[4]== current && state[6] == current) {
+        console.log(`joueur ${current} a gagné`);
+        
+    } 
+}
+
+function stop() {
+    if (win == true) {
+        console.log('PARTIE FINIE');//verif ok
+
+    }
+}
