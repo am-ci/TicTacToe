6e4f76bb484e204ea754c60f8611164dc9d8d092
Amandine Cian <amandine.cian@gmail.com>
2022-12-15 12:23:29 +0100

    tableau

--- script-tictac.js
+++ script-tictac.js
@@ -1,11 +1,22 @@
 let cells = document.querySelectorAll('.cell');
+let state = ["","","","","","","","",""];
+let current = 'X';
+
 
 for (let cell of cells) {
     cell.addEventListener('click', function(){
         console.log(cell.dataset.game) //verif
-
+        if (state[cell.dataset.game -1] == "") {
+            state[cell.dataset.game -1] = current
+            console.log(state);
+        } else {
+            alert ("ERREUR")
+        }
     });
 
 }
 
 // cell.dataset.game // dataset.cell équivaut à data-game de mon HTML
+
+
+
